c41b8456ef34493782e29ba66e8e0a58ff3a830a
MJ Sidik <darkimanity@gmail.com>
2024-09-17 16:15:02 +0700

    Update controller

--- Assets/Code/PlayerMovement.cs
+++ Assets/Code/PlayerMovement.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    float speed = 0.25f;
+    float dirX;
+    float dirY;
+    [SerializeField] Transform cameraFollow;
+    [SerializeField] Transform sprite_karakter;
+    [SerializeField] Animator animator_karakter;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        cameraFollow.transform.position = new Vector3(transform.position.x, transform.position.y, -10f);
+
+        FlipSprite();
+
+        if (dirX != 0 || dirY != 0)
+        {
+            animator_karakter.SetBool("Walk", true);
+        }
+        else
+        {
+            animator_karakter.SetBool("Walk", false);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        dirX = Input.GetAxis("Horizontal") * speed;
+        dirY = Input.GetAxis("Vertical") * speed;
+
+        transform.Translate(dirX, dirY, 0);
+    }
+
+    void FlipSprite()
+    {
+        if (dirX < 0)
+        {
+            sprite_karakter.localScale = new Vector2(-1f, 1);
+        }
+        else
+        {
+            sprite_karakter.localScale = new Vector2(1f, 1);
+        }
     }
 }
