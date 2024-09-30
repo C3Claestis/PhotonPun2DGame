213bb4ee155776120edb3d851af2d2e6eb38157b
MJ Sidik <darkimanity@gmail.com>
2024-09-30 12:45:06 +0700

    Update 30 september

--- Assets/Matkul/NetworkManajer.cs
+++ Assets/Matkul/NetworkManajer.cs
@@ -16,24 +16,34 @@ public class NetworkManajer : MonoBehaviourPunCallbacks
 
     [Header("Room Panel")]
     [SerializeField] GameObject RoomPanel;
-    
+    [SerializeField] InputField namaRoomInputfield;
+    [SerializeField] InputField MaxPlayerInputfield;
+
     [Header("Game Panel")]
     [SerializeField] GameObject GamePanel;
+    [SerializeField] Text roomInfoText;
+    [SerializeField] GameObject TombolMulaiGame;
 
     [Header("Create Room Panel")]
     [SerializeField] GameObject CreateRoomPanel;
 
     [Header("Show Room Panel")]
     [SerializeField] GameObject DaftarRoomPanel;
+    [SerializeField] GameObject daftarEntriRoomPrefab;
+    [SerializeField] GameObject daftarRoomUtamaGameobject;
 
     [Header("Join Room")]
     [SerializeField] GameObject JoinRoom;
+    private Dictionary<string, RoomInfo> cachedDaftarRoom;
+    private Dictionary<string, GameObject> daftarRoomGameobjects;
 
     // Start is called before the first frame update
     void Start()
     {
         // Pastikan panel login terlihat di awal
         ActivePanel(GamePanel.name);
+        cachedDaftarRoom = new Dictionary<string, RoomInfo>();
+        daftarRoomGameobjects = new Dictionary<string, GameObject>();
     }
 
     // Update is called once per frame
@@ -56,18 +66,121 @@ public class NetworkManajer : MonoBehaviourPunCallbacks
             PhotonNetwork.ConnectUsingSettings();
         }
     }
+    public void OnRoomCreateButtonClicked()
+    {
+        string namaRoom = namaRoomInputfield.text;
 
+        if (string.IsNullOrEmpty(namaRoom))
+        {
+            namaRoom = "Room " + Random.Range(1000, 10000);
+        }
+
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = (byte)int.Parse(MaxPlayerInputfield.text);
+
+        PhotonNetwork.CreateRoom(namaRoom, roomOptions);
+    }
+    public void OnCancelButtonClicked()
+    {
+        ActivePanel(RoomPanel.name);
+    }
     public override void OnConnected()
     {
         Debug.Log("Connecting Complete");
     }
+    public override void OnCreatedRoom()
+    {
+        Debug.Log(PhotonNetwork.CurrentRoom.Name + " Berhasil Membuat Room");
+    }
+    public override void OnJoinedRoom()
+    {
+        Debug.Log(PhotonNetwork.LocalPlayer.NickName + " Bergabung dengan " + PhotonNetwork.CurrentRoom.Name);
+        ActivePanel(GamePanel.name);
 
+        if (PhotonNetwork.LocalPlayer.IsMasterClient)
+        {
+            TombolMulaiGame.SetActive(true);
+        }
+        else
+        {
+            TombolMulaiGame.SetActive(false);
+        }
+
+        roomInfoText.text = "Nama Room : " + PhotonNetwork.CurrentRoom.Name + " " + "Maks.players : " + PhotonNetwork.CurrentRoom.PlayerCount + "/" + PhotonNetwork.CurrentRoom.MaxPlayers;
+    }
     public override void OnConnectedToMaster()
     {
         Debug.Log(PhotonNetwork.LocalPlayer.NickName + " Terhubung Ke Server");
         ActivePanel(RoomPanel.name);
     }
 
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        ClearRoomListView();
+
+        foreach (RoomInfo room in roomList)
+        {
+            Debug.Log(room.Name);
+            if (!room.IsOpen || !room.IsVisible || room.RemovedFromList)
+            {
+                if (cachedDaftarRoom.ContainsKey(room.Name))
+                {
+                    cachedDaftarRoom.Remove(room.Name);
+                }
+            }
+            else
+            {
+                if (cachedDaftarRoom.ContainsKey(room.Name))
+                {
+                    cachedDaftarRoom[room.Name] = room;
+                }
+                else
+                {
+                    cachedDaftarRoom.Add(room.Name, room);
+                }
+            }
+        }
+
+        foreach (RoomInfo roomInfo in cachedDaftarRoom.Values)
+        {
+            GameObject daftarEntriRoomGameobject = Instantiate(daftarEntriRoomPrefab);
+            daftarEntriRoomGameobject.transform.SetParent(daftarRoomUtamaGameobject.transform);
+            daftarEntriRoomGameobject.transform.localScale = Vector3.one;
+
+            daftarEntriRoomGameobject.transform.Find("NamaRoomText").GetComponent<Text>().text = roomInfo.Name;
+            daftarEntriRoomGameobject.transform.Find("MaxPlayerText").GetComponent<Text>().text = roomInfo.PlayerCount + "/" + roomInfo.MaxPlayers;
+            daftarEntriRoomGameobject.transform.Find("TombolGabung").GetComponent<Button>().onClick.AddListener(() => OnJoinRoomButtonClicked(roomInfo.Name));
+
+            daftarRoomGameobjects.Add(roomInfo.Name, daftarEntriRoomGameobject);
+        }
+    }
+    public void OnShowRoomListButton()
+    {
+        if (!PhotonNetwork.InLobby)
+        {
+            PhotonNetwork.JoinLobby();
+        }
+
+        ActivePanel(DaftarRoomPanel.name);
+    }
+    void OnJoinRoomButtonClicked(string _namaroom)
+    {
+        if (PhotonNetwork.InLobby)
+        {
+            PhotonNetwork.LeaveLobby();
+        }
+
+        PhotonNetwork.JoinRoom(_namaroom);
+    }
+    void ClearRoomListView()
+    {
+        foreach (var daftarRoomGameobjects in daftarRoomGameobjects.Values)
+        {
+            Destroy(daftarRoomGameobjects);
+        }
+
+        daftarRoomGameobjects.Clear();
+    }
     public void ActivePanel(string panelToBeActived)
     {
         panelLogin.SetActive(panelToBeActived.Equals(panelLogin.name));
